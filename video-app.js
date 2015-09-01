0b0af32d4bbdc7ac0d06edb42dc497f43c61d9b4
Stefan Lissia <stefan.emailacct@gmail.com>
2015-09-01 14:13:49 +0100

    formatting time inside of controller

--- video-app.js
+++ video-app.js
@@ -70,3 +70,36 @@ videoApp.controller('VideoController', ['$scope','$window', function($scope,$win
 
 
 }]);
+
+
+videoApp.filter('time', function() {
+	return function(seconds) {
+		var hh = Math.floor(seconds/ 3600),
+		mm = Math.floor(seconds/ 60),
+		ss = Math.floor(seconds) % 60;
+
+		return hh + ":" + (mm < 10 ? "0" : "") + mm + ":" + (ss < 10 ? "0" : "") +ss;
+
+
+	};
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
