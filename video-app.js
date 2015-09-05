e581548e9ccc56e968d3c6c54eb8f281e346ee2c
Stefan Lissia <stefan.emailacct@gmail.com>
2015-09-05 23:01:49 +0100

    binding some properties dynamically

--- video-app.js
+++ video-app.js
@@ -2,9 +2,11 @@ var videoApp = angular.module('videoApp', []);
 
 
 
-videoApp.controller('VideoController', ['$scope', function($scope) {
-	$scope.test = 'testing angular ok';
-
+videoApp.controller('VideoController', ['$scope', '$window', function($scope,$window) {
+	$scope.videoDisplay = document.getElementById('VideoElement');
+	$scope.videoSource = $window.videoSource;
+	$scope.titleDisplay = $window.titleDisplay;
+	$scope.videoDescription = $window.videoDescription;
 
 }]);
 
