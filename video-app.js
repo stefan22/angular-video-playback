8d756147cf69131923e78c0bd51faaa063c5a69a
Stefan Lissia <stefan.emailacct@gmail.com>
2015-09-05 22:05:27 +0100

    wires video controls using javascript

--- video-app.js
+++ video-app.js
@@ -1,4 +1,17 @@
+var v = document.getElementById('VideoElement');
 
+function playVideo() {
+	v.play();
+}
+
+function pauseVideo() {
+	v.pause();
+}
+
+function stopVideo() {
+	v.pause();
+	v.currentTime = 0;
+}
 
 
 
