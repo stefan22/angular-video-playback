2cb9a200c57380dff2f550d9dcc024b9cab34192
Stefan Lissia <stefan.emailacct@gmail.com>
2015-09-06 20:43:37 +0100

    adds function that formats time and then filters it in the index file

--- video-app.js
+++ video-app.js
@@ -93,4 +93,15 @@ videoApp.controller('VideoController', ['$scope', '$window', function($scope,$wi
 
 
 
+videoApp.filter('time', function() {
+	return function(seconds) {
+		var hh = Math.floor(seconds/ 3600),
+		mm = Math.floor(seconds/ 60),
+		ss = Math.floor(seconds) % 60;
 
+		return hh + ":" + (mm < 10 ? "0" : "") + mm + ":" + (ss < 10 ? "0" : "") +ss;
+
+
+	};
+
+});
