02b746506b26181b6ac2b7efbbf76b7b5e05bb57
Stefan Lissia <stefan.emailacct@gmail.com>
2015-09-06 17:48:37 +0100

    converts javascript to angular via scope and ng click

--- video-app.js
+++ video-app.js
@@ -3,10 +3,48 @@ var videoApp = angular.module('videoApp', []);
 
 
 videoApp.controller('VideoController', ['$scope', '$window', function($scope,$window) {
-	$scope.videoDisplay = document.getElementById('VideoElement');
+	$scope.videoDisplay = document.getElementById('videoElement');
 	$scope.videoSource = $window.videoSource;
 	$scope.titleDisplay = $window.titleDisplay;
 	$scope.videoDescription = $window.videoDescription;
+
+	$scope.videPlaying = false;
+
+	$scope.togglePlay = function() {
+		if($scope.videoDisplay.paused) {
+			$scope.videoDisplay.play();
+			$scope.videoPlaying = true;
+		} else {
+			$scope.videoDisplay.pause();
+			$scope.videoPlaying = false;
+		}
+
+
+	}
+
+
+	$scope.toggleMute = function() {
+		if($scope.videoDisplay.volume = 0.0) {
+			$scope.videoDisplay.volume = 1.0;
+		} else {
+			$scope.videoDisplay.volume = 0.0;
+		}
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 
 }]);
 
